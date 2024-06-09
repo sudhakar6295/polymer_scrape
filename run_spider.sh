c05bd91eeb0679bf0b2738121f1e31ce62f7a121
sudhakar6295 <53999168+sudhakar6295@users.noreply.github.com>
2024-06-09 08:15:00 +0530

    Added run_spider.sh

---
+++ run_spider.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Activate virtual environment
+source /home/polymer/python3.8/bin/activate
+
+# Navigate to your Scrapy project directory
+cd /home/polymer/polymer_scrape
+
+# Run Scrapy spider
+scrapy crawl polymershapes
+
+
+
+
+
