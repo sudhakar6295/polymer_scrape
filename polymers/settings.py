d80593ceb3112a0b3cce0a72967c28d3c7a5b051
sudhakar6295 <53999168+sudhakar6295@users.noreply.github.com>
2024-07-28 18:09:56 +0530

    Update settings.py

--- polymers/settings.py
+++ polymers/settings.py
@@ -91,6 +91,3 @@ ITEM_PIPELINES = {
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
-ITEM_PIPELINES = {
-    'polymers.pipelines.PolymersPipeline': 300,
-}
